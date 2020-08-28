1b663dcabed0b5ea5e9bbb469c9ab434beae1d9f
quanle <leminhquan8101998@gmail.com>
2020-08-28 14:27:52 +0700

    Set the routes constraints for the api

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,4 @@
-Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+MarketPlaceApi::Application.routes.draw do
+  namespace :api , defaults: { format: :json }, path: '/' do
+  end
 end
