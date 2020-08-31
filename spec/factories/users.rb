645fff95964e70a2b6d75eef45d0d04002218f5d
quanle <leminhquan8101998@gmail.com>
2020-08-31 10:09:24 +0700

    Adds user firsts specs

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,5 +1,7 @@
 FactoryBot.define do
-  factory :user do
-    
+  factory :user, class:User do
+    email { FFaker::Internet.email }
+    password {"123456789"}
+    password_confirmation {"123456789"}
   end
 end
