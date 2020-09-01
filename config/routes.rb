b0a7155cad075aa8bfa4a5aa9bc9f7caa236c16b
quanle <leminhquan8101998@gmail.com>
2020-09-01 16:08:39 +0700

    Adds the user create endpoint

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,7 @@ Rails.application.routes.draw do
   namespace :api , defaults: { format: :json }, path: '/' do
     scope module: :v1 do
       # constraints: ApiConstraints.new(version: 1, defaults: true) do
-        resources :users, :only => [:show]
+        resources :users, :only => [:show, :create]
     end
   end
 end
