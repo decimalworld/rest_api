eebb81f9d93a9213c46882c50363ed76fd9f02ed
quanle <leminhquan8101998@gmail.com>
2020-09-01 13:48:16 +0700

    Adds show action the users controller

--- config/routes.rb
+++ config/routes.rb
@@ -3,8 +3,9 @@ require 'api_constraints'
 Rails.application.routes.draw do
   devise_for :users
   namespace :api , defaults: { format: :json }, path: '/' do
-    scope module: :v1,
-      constraints: ApiConstraints.new(version: 1, defaults: true) do
+    scope module: :v1 do
+      # constraints: ApiConstraints.new(version: 1, defaults: true) do
+        resources :users, :only => [:show]
     end
   end
 end
