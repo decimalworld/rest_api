5a2fa4750dc6c2f496f66be054de7f606569ed39
quanle <leminhquan8101998@gmail.com>
2020-09-03 10:16:32 +0700

    Refactors the json parse method

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -13,6 +13,26 @@
 # it.
 #
 # See http://rubydoc.info/gems/rspec-core/RSpec/Core/Configuration
+
+ENV['RAILS_ENV'] ||= 'test'
+require File.expand_path('../../config/environment', __FILE__)
+
+abort("The Rails environment is running in production mode!") if Rails.env.production?
+require 'spec_helper'
+require 'rspec/rails'
+
+# with many helpers
+Dir[File.dirname(__FILE__) + "/support/*.rb"].each {|f| require f }
+# or only one   
+# require 'support/request_helpers'
+
+ActiveRecord::Migration.maintain_test_schema!
+
+
+# require 'spec_helper'
+# require 'rspec/rails'
+# Dir[Rails.root.join("spec/support/**/*.rb")].each { |f| require f }
+
 RSpec.configure do |config|
   # rspec-expectations config goes here. You can use an alternate
   # assertion/expectation library such as wrong or the stdlib/minitest
@@ -43,7 +63,7 @@ RSpec.configure do |config|
   # inherited by the metadata hash of host groups and examples, rather than
   # triggering implicit auto-inclusion in groups with matching metadata.
   config.shared_context_metadata_behavior = :apply_to_host_groups
-
+  config.include Request::JsonHelpers, :type => :controller
 # The settings below are suggested to provide a good initial experience
 # with RSpec, but feel free to customize to your heart's content.
 =begin
