3da006ab47694c51ffbc8f5037e72ff1a4cd1417
quanle <leminhquan8101998@gmail.com>
2020-09-08 18:01:29 +0700

    Adds the create method for the orders controller

--- spec/controllers/api/v1/orders_controller_spec.rb
+++ spec/controllers/api/v1/orders_controller_spec.rb
@@ -32,4 +32,36 @@ RSpec.describe Api::V1::OrdersController do
 
     it { should respond_with 200 }
   end
+
+  describe "POST #create" do
+    before(:each) do
+      current_user = FactoryBot.create :user
+      api_authorization_header current_user.auth_token
+      
+      product_1 = FactoryBot.create :product
+      product_2 = FactoryBot.create :product
+      order_params = { product_ids: [product_1.id, product_2.id] }
+        post :create, params: {user_id: current_user.id, order: order_params}
+    end
+
+    it "returns just the user order record" do
+      order_response = json_response[:data]
+      expect(order_response[:id]).to be_present
+    end
+
+    it { should respond_with 201 }
+  end
+
+  describe '#set_total!' do
+    before(:each) do
+      product_1 = FactoryBot.create :product, price: 100
+      product_2 = FactoryBot.create :product, price: 85
+
+      @order = FactoryBot.create :order, product_ids: [product_1.id,product_2.id]
+    end
+
+    it "returns the total amount to pay for the products" do
+      expect{@order.set_total!}.to change{@order.total}.from(0).to(185)
+    end
+
 end
